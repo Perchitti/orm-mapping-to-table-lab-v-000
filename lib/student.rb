506f32cce5aefaad2f7c67112fba2c101a840c7f
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-04 16:59:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,12 @@ def save
   DB[:conn].execute(sql, self.name, self.grade)
 
 end
+
+def self.create(name:, grade:)
+student = Student.new(name, grade)
+student.save
+student
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
